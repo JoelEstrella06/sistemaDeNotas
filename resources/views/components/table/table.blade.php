56c7935702e734ced971a35da2ac406703d0c150
Joel Estrella <joelestrella06@gmail.com>
2025-03-01 17:42:37 -0600

    style: cambio de color a tabla

--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -1,5 +1,5 @@
 <table class="text-sm w-full block md:table">
-    <thead class="bg-green-800 text-green-50 hidden md:table-header-group">
+    <thead class="bg-blue-950 text-blue-50 hidden md:table-header-group">
         {{$head}}
     </thead>
     <tbody class="md:divide-y text-balance flex flex-col gap-3 md:table-row-group">
