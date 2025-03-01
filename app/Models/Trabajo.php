59cd6dce0afd7fa4752f83b50f6b26cf728c83d2
Joel Estrella <joelestrella06@gmail.com>
2025-03-01 17:40:55 -0600

    feat(back): add mutador para el titulo

--- app/Models/Trabajo.php
+++ app/Models/Trabajo.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Trabajo extends Model
 {
     use SoftDeletes;
+
+    //mutador titulo
+    public function title():Attribute
+    {
+        return Attribute::make(
+            set: fn (string $value) => mb_strtoupper($value),
+        );
+    }
 }
