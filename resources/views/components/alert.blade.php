aea0d37ce0ed1322903b026490c029dbdcd5b3d8
Joel Estrella <joelestrella06@gmail.com>
2025-03-02 16:55:16 -0600

    feat(front): cambios en componente de alertas

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -10,7 +10,20 @@
         $clases="text-red-800 bg-red-200 z-50";
     }
 @endphp
-@session('success','error')    
+@session('success')    
+  <div class="flex items-center p-2 sm:p-3 text-sm w-full fixed top-0 {{$clases}}":class="close ?'hidden':''" x-data="{close:false}">
+    <div class="flex items-center gap-2 max-w-7xl m-auto">
+      <x-icons.info class="size-6"/>
+      <div>
+        <p class="font-medium">{{$message}}</p>
+      </div>
+    </div>
+      <button class="p-2 flex justify-center items-center" @click="close=!close">
+        <x-icons.close class="size-6"/>
+      </button>
+  </div>
+@endsession
+@session('error')    
   <div class="flex items-center p-2 sm:p-3 text-sm w-full fixed top-0 {{$clases}}":class="close ?'hidden':''" x-data="{close:false}">
     <div class="flex items-center gap-2 max-w-7xl m-auto">
       <x-icons.info class="size-6"/>
