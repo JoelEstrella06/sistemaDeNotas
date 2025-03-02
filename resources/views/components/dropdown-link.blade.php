7d6eb694987f7b254bac6b00be96ceb3bbd82420
Joel Estrella <joelestrella06@gmail.com>
2025-03-02 16:56:37 -0600

    feat(fornt) add iconos y cambios de estilos

--- resources/views/components/dropdown-link.blade.php
+++ resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->merge(['class' => 'block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'block px-4 py-2 transition duration-300 text-sm text-gray-500 w-full hover:bg-gray-100 hover:text-purple-700 font-medium']) }}>{{ $slot }}</a>
