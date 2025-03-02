de3c9f95c2eecd58d84cb21244db959b876f50bd
Joel Estrella <joelestrella06@gmail.com>
2025-03-02 16:57:18 -0600

    feat(back): add scopes y relaciones enrtre modelos

--- app/Models/Remision.php
+++ app/Models/Remision.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Remision extends Model
 {
     use SoftDeletes;
+
+    public function scopeSearch(Builder $query,string $search){
+        return $query->where('num_unidad','like','%'.$search.'%')
+                    ->orWhere('comentario','like','%'.$search.'%');
+    }
+
+    public function remision_trabajos():HasMany
+    {
+        return $this->hasMany(RemisionTrabajo::class);
+    }
 }
--- app/Models/RemisionTrabajo.php
+++ app/Models/RemisionTrabajo.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class RemisionTrabajo extends Model
 {
     use SoftDeletes;
+
+    public function trabajo():BelongsTo
+    {
+        return $this->belongsTo(Trabajo::class);
+    }
 }
